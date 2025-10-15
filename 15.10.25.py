ea7074d6106c47f1c47bef2245ae7f2ec76b75bb
liza_kryachko <krckoliza095@gmail.com>
2025-10-15 19:36:27 +0200

    lalalalala

---
+++ 15.10.25.py
@@ -0,0 +1,92 @@
+#a = int(input())
+#print(True if a % 5 == 0 else False)
+#действие иф условие елс действие
+
+#2
+
+#a = int(float(input()))
+#print("yes" if 10 <= a <= 20 else "no")
+
+# while - пока
+# бесконечный цикл если true
+# while True: #while условие ( а > 10 )
+   # print("hello")
+
+# for перменная in итерируемый объект
+# list
+#1:1
+#3:3
+#2:2
+#string
+#a = "fbkbkgsgj"
+# print (a[0])
+# в перменную i записываются элементы посимвольно
+# для каждой итерации цикла
+
+#for i in a :
+ #   print(i)
+
+
+ #range (начало, конец, шаг)
+ #генерирует список(генератор)чисел
+ #начало до конец - 1
+ #print(list(range(0,10)))
+ #print(list(range(0,10,3)))
+ #print(list(range(10,0,-1)))
+ #for i in [1,2,3,4,5,6,7,8,9,10]:
+  #   print(i)
+
+#for a in range(10):
+#    print(a)
+
+#break - останавливает выполнения цикла
+#continue - останавливает итерацию цикла
+#counter = 0
+#counter_false = 0
+#while True:
+#    counter +=1
+#    if counter % 3 != 0:
+ #       continue
+ #   print("hello", counter)
+ #   if counter >= 100:
+ #       break
+#    print("это колво не кратных чисел:",counter_false)
+
+
+#a = [[1,2,3],[4,5,6],[7,8,9]]
+#for i in a:
+ #   for j in i:
+  #   print(j)
+#a = [[1,2,3],[4,5,6],[7,8,9]]
+#i = 0
+#j = 0
+#print(len(a[0]))
+#len() - возвращает длину объекта
+#print(a[3])
+#print(a[len(a)-1])
+#a = [1,2,3]
+#print(a[-1])#отрицательный индекс получает элемент с обратной стороны
+#while i < len(a) :
+ #   while j < len(a[0]):
+  #   print(a[i][i])
+   # j+=1
+    #i+=1
+
+ #1
+#for i in range(0,11):
+ #   print(i)
+
+#6
+
+#a = int(input())
+#for i in range(1,n):
+ #   print(i**2)
+
+#11
+
+#for i in range (1,20):
+ #   if i % 3 == 0:
+  #      continue
+   # print(i)
+#print("gotovo")
+
