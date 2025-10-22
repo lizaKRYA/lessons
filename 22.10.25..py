d2a6d47173ea6f445ad648b7a9640cfe07e2bc2f
lizaKRYA <elizavetakryachko@icloud.com>
2025-10-22 11:33:08 +0300

    cccc

---
+++ 22.10.25..py
@@ -0,0 +1,40 @@
+#1
+
+#-
+
+#2
+
+#a = input()
+#for i in range(1,5):
+#    for j in range(-1,-2,-1):
+#        print("*"*i+" "*i)
+#while True:
+#  if a == "yes":
+#    print()
+#    break
+#  elif a != "yes":
+#    print("no")
+
+#3
+#while True:
+#   a = int(input())
+#   c = int(input())
+#   b = input()
+#   if b == "stop":
+#    print("goodbye")
+#    break
+#if b == "+":
+#    result = a + c
+#elif b == "-":
+#    result = a - c
+#elif b == "*":
+#    result = a * c
+#elif b == "/":
+#    result = a / c
+#    print(result)
+#else:
+#    print("error'")
+
+#4
+
+#-
