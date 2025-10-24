a582e62ae3ca56294025f69c8a2aaf1f93c9fd16
lizaKRYA <elizavetakryachko@icloud.com>
2025-10-24 22:35:47 +0300

    oa

---
+++ 25.10.25hw.py
@@ -0,0 +1,50 @@
+#1
+
+#for i in range(100,9,-10):
+#    print(i)
+
+#2
+
+
+#while True:
+#  a = int(input())
+#  if a % 5 == True:
+#   print("кратное 5")
+#   break
+#  else:
+#    print("введено число")
+
+#3
+
+#n = int(input())
+#for i in range(1, n + 1):
+#  for j in range(1, n + 1):
+#    k = i * j
+#    print(f"{i}*{j}={k}")
+
+#4
+
+#a = int(input())
+#b = int(input())
+#j = 0
+#if a<=b:
+#  for i in range(a, b +1):
+#    j +=  i **2
+#    print(j)
+#else:
+#  print("error")
+
+#5
+
+#h = int(input())
+#for i in range(1, h + 1):
+#  for j in range(h - 1):
+#    print(" ", end=" ")
+#    for k in range(1, i + 1):
+#      print(k, end=" ")
+#      for y in range(i - 1, 0, -1):
+#        print(y,end=" ")
+
+#6
+
+#вообще не поняла
