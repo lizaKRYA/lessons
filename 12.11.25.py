84b185eca3849eeb99a3eacc64230c4bd021b44b
lizaKRYA <elizavetakryachko@icloud.com>
2025-11-12 14:26:12 +0300

    12345678

---
+++ 12.11.25.py
@@ -0,0 +1,18 @@
+#1
+#f = 14, 2, 72, 93, 45, 28, 13, 99, 27, 36, 19, 59, 15, 74, 71
+#min_f = f.index(min(f))
+#max_f = f.index(max(f))
+#if max_f > min_f:
+#    result_f = f[min_f:max_f]
+#else:
+#    result_f = f[max_f:min_f]
+#    print(result_f)
+#не выводится :(
+
+
+#2
+
+#-
+
+
+
