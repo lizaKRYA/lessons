9c9a44b5f50379392a1cc61023c87047327adb3f
lizaKRYA <elizavetakryachko@icloud.com>
2025-11-15 15:50:51 +0300

    homework

---
+++ 15.11.25hw.py
@@ -0,0 +1,40 @@
+#1
+
+#set1 = {1, 2, 3}
+#set2 = {4, 5, 6}
+#set_3 = set1.union(set2)
+#print(set_3)
+
+#2
+
+#fruits = ["яблоко", "банан", "яблоко",
+#"апельсин", "банан", "киви"]
+#fruits_unique = set(fruits)
+#print(len(fruits_unique))
+
+#3
+
+#A = {1, 2, 3, 4}
+#B = {3, 4, 5, 6}
+#C = A.intersection(B)
+#print(C)
+
+#4
+
+#f = ("tea","apple","banana")
+#a,b,c = tuple(f)
+#print(b)
+
+#5
+
+#g = (1,2,3,4,5,6,7,8,9,4,3,2,5,7,4,3,5,7,8,1)
+#s,*s1 = tuple(g)
+#print(s)
+#print(s1)
+
+#6
+
+#t = (1,2)
+#t1 = (3,4)
+#t_0 = t + t1
+#print(t_0)
