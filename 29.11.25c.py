de2f580c56d69eb3f929614d08772f004597e359
lizaKRYA <elizavetakryachko@icloud.com>
2025-11-29 22:58:37 +0300

    classwork

---
+++ 29.11.25c.py
@@ -0,0 +1,60 @@
+#homework
+#a = ("hello")
+#b = ""
+#for i in range(len(a)):
+#    if a[i].isupper():
+#        b += "_" + a[i]
+#    else:
+#        b += a[i]
+#print(b.lower())
+
+#11
+
+#def a(f_str):
+#    sentenses = f_str.count(".") + f_str.count("!") + f_str.count("?")
+#    words = len(f_str.split(""))
+#    symbols = len(f_str)
+#    return (sentenses, words, symbols)
+#print(a("hello.worlds"))
+
+#21
+
+#def calculate_area(shape = "rectangle", length = 0, width = 0, radius = 0):
+#    if shape == "rectangle":
+#        return length * width
+#    elif shape == "circle":
+#        return 3.14 * (radius ** 2)
+#    print
+
+#25
+
+#def formar_name(first, last, middle= "", title = ""):
+#    return title + " " + first + " " + middle + " " + last
+#print(format_name("ll", "kk", title = " mr"))
+
+#23
+
+#def func1(*args):
+#    return list(set(args))
+#print(func1(1, 2, 3))
+
+#29
+
+#from random import randint
+#def func2(k,f):
+#    list_func = []
+#    marged_str = ""
+#    for i in range(k,f):
+#        list_func2.append(randint(1,100))
+#        merged_str += str(z)
+#        maxim = 0
+#        for i in merged_str:
+#            value_cash = list_func.count(i)
+#maxim = max(maxim, value_cash)
+#            if maxim < value_cash:
+#                maxim = 0
+#                digit  = -1
+#                maxim = value_cash
+#                return list_func, digit, maxim
+#            print(func2(1,100))
+
