25e2d70521cbd9c8a7bd785fc7cf904c803afb87
lizaKRYA <elizavetakryachko@icloud.com>
2025-12-03 15:58:56 +0300

    homework

---
+++ 03.12.25.py
@@ -0,0 +1,22 @@
+#1
+
+#def get_digits(n):
+#    if n >= 0:
+#     return list(map(int, str(n)))
+#print(get_digits(123))
+
+#2
+
+#def count_words(text, ignore_case=True, ignore_punctuation=False):
+#    word_count = {}
+#    if ignore_case:
+#     text = text.lower()
+#     words = text.split()
+#    for word in words:
+#        if word in word_count:
+#            word_count += 1
+#        else:
+#            word_count = 1
+#            return word_count
+#        print(count_words(word, ignore_punctuation))
+
