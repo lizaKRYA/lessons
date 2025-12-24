3553f6964eb0e39bb10fb9af9f6100bd49718714
lizaKRYA <elizavetakryachko@icloud.com>
2025-12-24 10:40:17 +0300

    homework

---
+++ 24.12.25.py
@@ -0,0 +1,25 @@
+#homework
+
+#1 : способ 1
+
+#b = input()
+#a = ""
+#for i in b:
+#    if i.isupper():
+#        a += chr(ord(i) + 32)
+#    else:
+#        a += i
+#print(a)
+
+#1 : способ 2
+
+#b = input()
+#a = ""
+#p = 0
+#while p < len(b):
+#    if b[p].isupper():
+#        a += chr(ord(b[p]) + 32)
+#    else:
+#        a += b[p]
+#    p += 1
+#print(a)
