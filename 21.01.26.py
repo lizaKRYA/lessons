af0f0e764f230eb1611d0b44ac28228272a315ad
lizaKRYA <elizavetakryachko@icloud.com>
2026-01-21 16:40:48 +0300

    homework

---
+++ 21.01.26.py
@@ -0,0 +1,32 @@
+#1
+
+#student = [("Анна", 16), ("Бронислав", 15), ("Федот", 18)]
+#min_ages = min(student, key=lambda x: x[1])
+#print(min_ages)
+
+#2
+
+#sigh = lambda x: 'positive' if x >= 0 else 'negative'
+#nums = [5, -3, 0]
+#for i in nums:
+#    print(sigh(i))
+
+#3
+'''
+add = lambda x,y: x + y
+substraction = lambda x, y: x - y
+
+def calculator(add1, substract1, num1, num2):
+    c = []
+    c.append(add1(num1, num2))
+    c.append(substraction(num1, num2))
+    return c
+result = calculator(add, substraction,10,4)
+
+print(result)
+'''
+
+#4
+
+#-
+#непонятно условие :(
